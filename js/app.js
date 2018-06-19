1304877d87d448ed55d3115d56dfc321e3e18a49
Bruno Li <lisiuloong@gmail.com>
2018-06-19 17:58:56 -0400

    refractor player prototype

--- js/app.js
+++ js/app.js
@@ -35,10 +35,10 @@ Enemy.prototype.update = function(dt) {
   // checks for enemies colision, character is draw back to the start if hit an enemy
   // source: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
   if (
-    (player.x < this.x + 60) &&
-    (player.x + 37 > this.x) &&
-    (player.y < this.y + 25) &&
-    (30 + player.y > this.y)
+    player.x < this.x + 60 &&
+    player.x + 37 > this.x &&
+    player.y < this.y + 25 &&
+    30 + player.y > this.y
   ) {
     player.x = 200; // draw character back to the start
     player.y = 385;
@@ -53,7 +53,7 @@ Enemy.prototype.render = function() {
 
 /* ---------------PLAYER CONSTRUCTOR AND ITS PROTOTYPE ------------------- */
 
-// Player constructor
+// Player Constructor
 let Player = function() {
   //draws character to start on the middle
   this.sprite = "images/char-boy.png";
@@ -62,63 +62,72 @@ let Player = function() {
   this.y = 385;
 }; // Player constructor
 
-Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  ctx.font = "30pt visitor"; // custom font
-  ctx.fillText("Score:" + " " + this.score, 0, 30); // Draw score board on the canvas
-}; //render
+ // Player Prototype
+Player.prototype = {
+  constructor: Player, // points to our Player constructor for inheritance
 
-Player.prototype.update = function() {
-  ctx.clearRect(0, 0, 500, 500);
-  //switches goal to true and add to score board
-  if (goal) {
-    this.score++;
-    goal = false; // switches goal to false again for the next score point
-  }
-}; // update
+  render: function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.font = "30pt visitor"; // custom font
+    ctx.fillText("Score:" + " " + this.score, 0, 30); // Draw score board on the canvas
+  }, // render
+   
+  // Update 
+  update: function() {
+    ctx.clearRect(0, 0, 500, 500);
+    //switches goal to true and add to score board
+    if (goal) {
+      this.score++;
+      goal = false; // switches goal to false again for the next score point
+    }
+  }, // update
 
-Player.prototype.handleInput = function(keyPress) {
-  if (keyPress === "left") {
-    if (this.x - 100 < 0) {
-      // dosent allow character to move off the left side of the grid
-      this.x = 0; // center the character on the middle of a block
-    } else {
-      this.x -= 100;
-    }
-  } else if (keyPress === "up") {
-    if (this.y - 85 < 0) {
-      goal = true;
-      player.update(); // call update method to update score
-      this.y = 380; // draw character back to the start
-    } else {
-      this.y -= 85; // draw the character and move up to step in every square according to y coordinate
-    }
-  } else if (keyPress === "right") {
-    if (this.x + 100 > 400) {
-      //dosent allow character to move off the right side of the grid
-      this.x = 400; // last coordinate of right block of the grid
-    } else {
-      this.x += 100; // draw character coordinates to the right squares every keyPress
-    }
-  } else if (keyPress === "down") {
-    if (this.y + 85 > 385) {
-      this.y = 385;
-    } else {
-      this.y += 85; // // draw character coordinates down to every squares for every keyPress
-    }
-  } // end of if/else statements
-}; // end of handleInput
+ // Event Listener
+  handleInput : function(keyPress) {
+    if (keyPress === "left") {
+      if (this.x - 100 < 0) {
+        // dosent allow character to move off the left side of the grid
+        this.x = 0; // center the character on the middle of a block
+      } else {
+        this.x -= 100;
+      }
+    } else if (keyPress === "up") {
+      if (this.y - 85 < 0) {
+        goal = true;
+        player.update(); // call update method to update score
+        this.y = 380; // draw character back to the start
+      } else {
+        this.y -= 85; // draw the character and move up to step in every square according to y coordinate
+      }
+    } else if (keyPress === "right") {
+      if (this.x + 100 > 400) {
+        //dosent allow character to move off the right side of the grid
+        this.x = 400; // last coordinate of right block of the grid
+      } else {
+        this.x += 100; // draw character coordinates to the right squares every keyPress
+      }
+    } else if (keyPress === "down") {
+      if (this.y + 85 > 385) {
+        this.y = 385;
+      } else {
+        this.y += 85; // // draw character coordinates down to every squares for every keyPress
+      }
+    } // end of if/else statements
+  }// end of handleInput
+};
+
+
+
+// Place the player object in a variable called player
+// instantiate new Player constructor
+let player = new Player();
 
 /* loops through the enemy location array, sets the starting position 0 for Y coordinates,
 then places the enemy location according to the array, and set the speed of the enemy
 crossing the screen by generating a random number with the help of update() method.
 */
 
-// Place the player object in a variable called player
-// instantiate new Player constructor
-let player = new Player();
-
-  // creates a new Enemy object with arguments for location and random speed and store inside enemy variable
+// creates a new Enemy object with arguments for location and random speed and store inside enemy variable
 const createEnemies = enemyLocation.forEach(function(location) {
   let enemy = new Enemy(0, location, 120 + Math.floor(Math.random() * 400));
   // push the created object to the array
@@ -126,10 +135,11 @@ const createEnemies = enemyLocation.forEach(function(location) {
 }); // forEach
 
 
+/* ---------------EVENT LISTENERS ------------------- */
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-// add extra keys for player to choose with what keys to play with
+// Add extra keys for player to choose with what keys to play with
 document.addEventListener("keyup", function(e) {
   let allowedKeys = {
     37: "left",
@@ -144,8 +154,8 @@ document.addEventListener("keyup", function(e) {
   player.handleInput(allowedKeys[e.keyCode]);
 }); // event listener
 
-// prevents the windows brower to scrool if arrow keys are pressed
-// source code from https://stackoverflow.com/questions/8916620/disable-arrow-key-scrolling-in-users-browser
+// Prevents the windows brower to scrool if arrow keys are pressed
+// Source code from https://stackoverflow.com/questions/8916620/disable-arrow-key-scrolling-in-users-browser
 window.addEventListener(
   "keydown",
   function(e) {
